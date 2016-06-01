850781e8e4d7d43fa14695efa15b73df84685fb1
Sergej Povzaniuk <liravanigroup@gmail.com>
2016-06-01 18:10:13 +0200

    Add files via upload

---
+++ JdbcClientRepository.java
@@ -0,0 +1,90 @@
+package pl.com.bottega.photostock.sales.infrastructure.repositories;
+
+import pl.com.bottega.photostock.sales.model.deal.Money;
+import pl.com.bottega.photostock.sales.model.exceptions.DataAccessException;
+import pl.com.bottega.photostock.sales.model.users.Client;
+import pl.com.bottega.photostock.sales.model.users.ClientFactory;
+import pl.com.bottega.photostock.sales.model.users.ClientStatus;
+import pl.com.bottega.photostock.sales.model.usertool.Reservation;
+
+import java.sql.*;
+import java.util.List;
+
+/**
+ * lublin-3-16-photostock
+ * Sergii
+ * 2016-06-01.
+ */
+
+public class JdbcClientRepository implements Repository<Client>{
+
+
+    private final String url;
+    private final String login;
+    private final String password;
+
+
+    public JdbcClientRepository(String url, String login, String password) throws SQLException {
+        super();
+        this.url = url;
+        this.login = login;
+        this.password = password;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, login, password);
+    }
+
+    @Override
+    public Client load(String number) {
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement("SELECT id, name, address, amount_cents, amount_currency, active, status FROM clients WHERE name = ?");
+            statement.setString(1, number);
+            ResultSet rs = statement.executeQuery();
+            if(rs.next()){
+                return ClientFactory.getClientInstance(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        rs.getString("address"),
+                        new Money(rs.getInt("amount_cents") / 100D, rs.getString("amount_currency")),
+                        ClientStatus.values()[rs.getInt("status")],
+                        rs.getBoolean("active")
+                );
+            }else{
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e, e.getMessage());
+        }
+    }
+
+    @Override
+    public void save(Client client) {
+        try (Connection connection = getConnection()) {
+
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO clients (name, address, amount_cents, amount_currency, active, status) VALUES(?,?,?,?,?,?)");
+            statement.setString(1, client.getName());
+            statement.setString(2, client.getAddress());
+            statement.setInt(3, client.getAmountCents());
+            statement.setString(4, client.getCurrency());
+            statement.setBoolean(5, client.isActive());
+            statement.setInt(6, client.getStatusIndex());
+
+            statement.execute();
+
+        } catch (SQLException e) {
+            throw new DataAccessException(e, e.getMessage());
+        }
+    }
+
+    @Override
+    public List find(String number) {
+        return null;
+    }
+
+    @Override
+    public Reservation findOpenedPer(Client client) {
+        return null;
+    }
+
+}
---
+++ JdbcClientRepositoryTest.java
@@ -0,0 +1,84 @@
+package pl.com.bottega.photostock.sales.infrastructure.repositories;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import pl.com.bottega.photostock.sales.model.deal.Money;
+import pl.com.bottega.photostock.sales.model.users.Client;
+import pl.com.bottega.photostock.sales.model.users.ClientFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+import static pl.com.bottega.photostock.sales.model.users.ClientStatus.STANDARD;
+
+
+public class JdbcClientRepositoryTest {
+    private Repository repo;
+    private final static Client INIT_CLIENT = ClientFactory.getClientInstance("1", "Agnieszka", "London", Money.FIVE_PL, STANDARD, true);
+
+    @Before
+    public void setUp() throws SQLException {
+        // given
+        Connection connection = DriverManager.getConnection("jdbc:hsqldb:mem:stockdb", "SA", "");
+        createClientsTable(connection);
+        insertTestClient(connection);
+        connection.close();
+        // when
+        repo = new JdbcClientRepository("jdbc:hsqldb:mem:stockdb", "SA", "");
+    }
+
+    @Test
+    public void shouldLoadClientByName() throws SQLException {
+        // when
+       Client loadedClient = (Client) repo.load("Agnieszka");
+
+        // then
+        assertEquals(INIT_CLIENT.getName(), loadedClient.getName());
+        assertEquals(INIT_CLIENT.getCurrency(), loadedClient.getCurrency());
+        assertEquals(INIT_CLIENT.getPaymentStrategy().getClass(), loadedClient.getPaymentStrategy().getClass());
+        assertEquals(INIT_CLIENT.getSaldo(), loadedClient.getSaldo());
+        assertEquals(Client.class, loadedClient.getClass());
+    }
+
+    @Test
+    public void shouldReturnNullWhenClientDoesNotExists() throws SQLException {
+        // when
+        Client client = (Client) repo.load("Mario");
+
+        //then
+        Assert.assertNull(client);
+    }
+
+    private void insertTestClient(Connection connection) throws SQLException {
+        connection.createStatement().executeUpdate("INSERT INTO Clients (name, address, amount_cents, amount_currency, status, active) VALUES ('Agnieszka','London', 500, 'PLN', 0, true);");
+    }
+
+    private void createClientsTable(Connection connection) throws SQLException {
+        connection.createStatement().executeUpdate("DROP TABLE clients IF EXISTS ");
+        connection.createStatement().executeUpdate("CREATE TABLE clients (\n" +
+                "  id INTEGER IDENTITY PRIMARY KEY,\n" +
+                "  name VARCHAR(255) NOT NULL,\n" +
+                "  address VARCHAR(255) NOT NULL,\n" +
+                "  amount_cents INTEGER DEFAULT 0 NOT NULL, \n" +
+                "  amount_currency CHAR(3) DEFAULT 'PLN' NOT NULL, \n" +
+                "  status INTEGER DEFAULT 0 NOT NULL, \n" +
+                "  active BOOLEAN DEFAULT true NOT NULL,  \n" +
+                ");");
+    }
+
+    @Test
+    public void shouldSaveClient(){
+        // given
+        Client savedClient = new Client("Jan Kowalski", "", "London", Money.FIVE_PL);
+        repo.save(savedClient);
+
+        //When
+        Client loadedClient = (Client) repo.load(savedClient.getName());
+
+        //then
+        Assert.assertEquals(savedClient.getName(),loadedClient.getName());
+    }
+}
